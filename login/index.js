2a041ae48cde43ec9e7549de78af17d440040cb2
Isabella Patrício <125597180+IsabellaPaty@users.noreply.github.com>
2023-09-21 10:14:53 -0300

    Update index.js

--- login/index.js
+++ login/index.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var dt = require('./login.js');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Isabella Maria Assis Patrício 19/09/2023');
-}).listen(8010);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("Login é legal: " + dt.myDateTime());
+  res.end();
+}).listen(5010);
+
