44bb31dd0c345a7279c3f0e71d448601ba5eb345
Isabella Patrício <125597180+IsabellaPaty@users.noreply.github.com>
2023-09-21 10:11:30 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Isabella Maria Assis Patrício 19/09/2023');
-}).listen(8010);
